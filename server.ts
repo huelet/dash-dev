ac1f9bac7ede7e725312c8022fdda21b748ca9c8
lerichardson <litbelb1@outlook.com>
2021-06-22 11:35:24 -0700

    Initial commit (unfinished)

---
+++ server.ts
@@ -0,0 +1,112 @@
+import express from "express";
+import * as dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+const rateLimit = require("express-rate-limit");
+const { BlobServiceClient } = require('@azure/storage-blob');
+const multer = require('multer');
+const multerAzure = require('multer-azure');
+const { v1: uuidv1 } = require('uuid');
+const cors = require('cors');
+const SSH = require('simple-ssh');
+import pug from 'pug';
+import path from 'path';
+const axios = require('axios');
+import { auth, requiresAuth } from 'express-openid-connect';
+const ssh = new SSH({
+    host: process.env.HOSTURL,
+    user: process.env.HOSTUNAME,
+    pass: process.env.HOSTPWD
+});
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    clientID: 'nRoiHn0wBQUbMvHjtYE1XkN0SQs4hQY3',
+    issuerBaseURL: 'https://huelet-cc.us.auth0.com',
+  };
+const limiter = new rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 3
+})
+const containerName = "asset-" + uuidv1();
+export async function createContainer() {
+  const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+  console.log('\n\tContainer Creation requested...');
+  console.log('\n\t\t', containerName);
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const createContainerResponse = await containerClient.create();
+  console.log("\n\t\t\tContainer was created successfully. requestId: ", createContainerResponse.requestId);
+}
+const uploadSettings = multer({
+  storage: multerAzure({
+    connectionString: process.env.AZURE_STORAGE_CONNECTION_STRING,
+    account: process.env.AZURE_STORAGE_ACCOUNT_NAME,
+    key: process.env.AZURE_STORAGE_KEY,
+    container: containerName
+  })
+})
+app.use(auth(config));
+app.use(cors());
+app.set('trust proxy', 1);
+app.set('view engine', 'pug')
+app.set('views', path.resolve('public'))
+app.get('/', (req: express.Request, res: express.Response) => {
+  // I can't think of any good variable names so screw it
+  const isAuthd = req.oidc.isAuthenticated()
+  res.render('home', {
+    isAuthd: isAuthd
+  })
+});
+app.get('/ul', requiresAuth(), (_req: express.Request, res: express.Response) => {
+  res.render('upload')
+})
+app.get('/studio/uploadSuccess', requiresAuth(), (req: express.Request, res: express.Response) => {
+  const videoUrl = req.query.cuurl;
+  if (videoUrl === undefined) {
+    res.json({ "error": "not all tokens were sent" })
+  } else {
+  res.render('success', { authorName: req.oidc.user.nickname })
+  }
+})
+app.get('/studio/videos', requiresAuth(), (_req: express.Request, res: express.Response) => {
+  res.json({ "hi, wanted to tell you this":"yes, i'm gonna make you load forever. anyways. cya." })
+})
+// Profile and settings
+app.get('/studio/me', requiresAuth(), (req: express.Request, res: express.Response) => {
+  const auth0id = req.oidc.user.sub;
+  const uid = auth0id.replace("auth0|", "")
+  res.render('profile', { pfp: req.oidc.user.picture, uname: req.oidc.user.nickname, email: req.oidc.user.email, uid: uid })
+})
+// Analytics (lord have mercy on my soul)
+app.get('/studio/analytics', requiresAuth(), (_req: express.Request, res: express.Response) => {
+  res.render('analytics')
+})
+// APIS GALORE
+app.get('/api/ul/cf', requiresAuth(), (req: express.Request, res: express.Response) => {
+    const videoTitle = req.query.videotitle;
+    const videoUrl = req.query.cuurl;
+    const author = req.query.authortitle;
+  const mailingAddress = req.oidc.user.email;
+    if (videoTitle === undefined || author === undefined || videoUrl === undefined || author != req.oidc.user.nickname) {
+        res.json({ error: 'not all tokens have been sent' })
+    }
+    ssh.exec(`cd /var/h && node cf.js --author "${author}" --title "${videoTitle}" --url "${videoUrl}"`, {
+        out: function (stdout: any) {
+            res.send(stdout);
+            console.log(stdout);
+            console.log(videoTitle);
+        }
+    }).start();
+});
+app.post('/api/ul/ul', requiresAuth(), uploadSettings.any(), (req, res, _next) => {
+    res.status(200).redirect(`/studio/uploadSuccess?cuurl=undefined`)
+});
+app.get(`/api/videos/list/newest`, requiresAuth(), (_req: express.Request, res: express.Response) => {
+  res.json({ "url": "https://huelet.net/w/pe3KhC40rENCtYcV/" });
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}, available at http://localhost:${port}`)
+})
