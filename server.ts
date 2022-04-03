b56e6739d473c76c58df6b1489daaef58c497aff
Levi <litbelb1@outlook.com>
2022-04-02 22:23:26 -0700

    Add login page + more reset updates

--- server.ts
+++ server.ts
@@ -1,32 +1,64 @@
 import express from "express";
 import path from "path";
-require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const rateLimit = require("express-rate-limit");
-const multer = require('multer');
-const multerAzure = require('mazure');
-const fetch = require('node-fetch');
-const poke = require('js.poke');
-const { v1: uuidv1 } = require('uuid');
-const cors = require('cors');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const axios = require("axios").default;
-const versionData = require('./verdata.json')
-const { useID } = require('@dothq/id');
-const { MeiliSearch } = require('meilisearch');
-import pug from 'pug';
+const versionData = require("./verdata.json");
+const { MeiliSearch } = require("meilisearch");
+import pug from "pug";
+require("dotenv").config();
 // Search init
-const search = new MeiliSearch({ host: 'https://search.huelet.net/', apiKey: '9941c5593840a732623257300eedf439f95a53d3c592a2e1e4ab143f86f25a24' });
+const search = new MeiliSearch({
+  host: "https://search.huelet.net/",
+  apiKey: "9941c5593840a732623257300eedf439f95a53d3c592a2e1e4ab143f86f25a24",
+});
 const limiter = new rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 3
+  max: 3,
 });
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('trust proxy', 1);
-app.set('view engine', 'pug');
-app.set('views', path.resolve('public'));
+app.use(cookieParser());
+app.set("trust proxy", 1);
+app.set("view engine", "pug");
+app.set("views", path.resolve("public"));
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  if (!req.cookies._hltoken) {
+    res.render("home", {
+      version: versionData.version,
+    });
+    return
+  } else if (req.cookies._hltoken) {
+    res.redirect("/dashboard");
+  }
+});
+app.get("/flow/in", (req: express.Request, res: express.Response) => {
+  if (!req.cookies._hltoken) {
+    res.render("login", {
+      version: versionData.version,
+    });
+    return
+  } else if (req.cookies._hltoken) {
+    res.redirect("/dashboard");
+  }
+});
+app.get("/upload", (req: express.Request, res: express.Response) => {
+  if (!req.cookies._hltoken) {
+    res.redirect("/");
+  } else if (req.cookies._hltoken) {
+    res.render("upload", {
+      version: versionData.version,
+    });
+  }
+})
+
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}, available at http://localhost:${PORT}`)
+  console.log(
+    `Server listening on port ${PORT}, available at http://localhost:${PORT}`
+  );
 });
--- src/flow/videos.ts
+++
@@ -1,7 +0,0 @@
-import { requiresAuth, auth } from "express-openid-connect";
-
-module.exports = requiresAuth(), (req: any, res: any) => {
-    const auth0id = req.oidc.user.sub;
-    const uid = auth0id.replace("auth0|", "");
-    res.redirect(`/studio/videos`, { uid: uid });
-};
