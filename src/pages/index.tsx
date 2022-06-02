63f7206308b424990c71b2493d1453409c89697c
Levi Richardson <litbelb1@outlook.com>
2022-06-02 20:46:36 +0000

    🔒️ Check for token on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import * as React from "react";
 import { css, jsx } from "@emotion/react";
+import { useCookies } from "react-cookie";
 
 const IndexPage = () => {
+  const [cookies, setCookie] = useCookies(["token"]);
+  fetch("https://api.huelet.net/auth/token", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${cookies.token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.response === "Success!") {
+        location.assign("/profile/me");
+      }
+    });
   return (
     <div id="root">
       <h2 className="text-big--h2">
