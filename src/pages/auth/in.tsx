e92db5175262fbf63ef138dcbe78afc12a48debb
Levi Richardson <litbelb1@outlook.com>
2022-06-02 18:55:28 +0000

    auth page init // update homepage

---
+++ src/pages/auth/in.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { css, jsx } from "@emotion/react";
+
+const LoginPage = () => {
+  return (
+    <div id="root">
+      <h2 className="text-big--h2">
+          
+      </h2>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,12 @@ import { css, jsx } from "@emotion/react";
 
 const IndexPage = () => {
   return (
-    <div
-      css={css`
-        display: flex;
-      `}
-    >
+    <div id="root">
+      <h2 className="text-big--h2">
+        if youre here, there's a good chance that you're looking for the{" "}
+        <a href="/auth/in">login page</a>. if you're not, you're probably in the
+        wrong place.
+      </h2>
     </div>
   );
 };
